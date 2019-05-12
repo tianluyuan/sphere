6b8a4e5de46a79143727064e9a5775c3c0e5f27f
Tianlu Yuan <tianlutheman@gmail.com>
2019-05-12 17:34:14 -0400

    update setup.py, rename for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sphere',
+    name='fb8',
     version='0.1.0',
     author='T. Yuan',
     author_email='tyuan@icecube.wisc.edu',
     description='Implementation of Kent (1982) and Bingham-Mardia (1978) distributions on a sphere',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/tianluyuan/sphere.git',
     packages=find_packages('./'),
     install_requires=['numpy',
