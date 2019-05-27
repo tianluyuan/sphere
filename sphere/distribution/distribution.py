fix normalization calculation for negative kappa/n1

sphere/distribution/distribution.py
@@ -340,10 +340,11 @@ class FB8Distribution(object):
                     while True:
                         # int sin(theta) dtheta
                         a = (
+                            # b**j * abs(0.5*k)**(-j - 0.5)
                             np.exp(
                                 np.log(b) * j +
-                                np.log(0.5 * k) * (-j - 0.5)
-                            ) * I(j + 0.5, k)
+                                np.log(abs(0.5 * k)) * (-j - 0.5)
+                            ) * I(j + 0.5, abs(k))
                         )
                         # int dphi
                         irange = np.arange(j + 1)
@@ -370,14 +371,15 @@ class FB8Distribution(object):
                         jj = 0
                         while True:
                             # int sin(theta) dtheta
-                            a = (n2**(2 * ll) * n3**(2 * kk) *
-                                (0.5 * k * n1)**(-jj -ll -kk -0.5)*
+                            a = (
+                                n2**(2 * ll) * n3**(2 * kk) *
+                                abs(0.5 * k * n1)**(-jj -ll -kk -0.5)*
                                 np.exp(
                                     np.log(b) * jj + np.log(k) * 2 * (ll+kk) -
                                     # np.log(n2) * 2 * ll + np.log(n3) * 2 * kk +
                                     # np.log(0.5 * k * n1) * (-jj -ll - kk - 0.5) -
                                     LG(2 * ll + 1) - LG(2 * kk + 1)
-                                ) * I(jj + ll + kk + 0.5, k*n1)
+                                ) * I(jj + ll + kk + 0.5, abs(k*n1))
                             )
                             # int dphi
                             irange = np.arange(jj + 1)
@@ -788,6 +790,15 @@ def fb8_mle(xs, verbose=False, return_intermediate_values=False, warning='warn',
         # note eta=-1 with 2*beta >= kappa is the small-circle distribution (Bingham-Mardia 1978)
         if verbose:
             __fb8_mle_output1(fb8(*y_start), callback)
+        ### constraint for SLSQP ###
+        # cons = ({"type": "ineq", # kappa >= 0
+        #          "fun": lambda x: x[3]},
+        #         {"type": "ineq", # beta >= 0
+        #          "fun": lambda x: x[4]},
+        #         {"type": "ineq", # -1 <= eta <=1
+        #          "fun": lambda x: 1 - np.abs(x[5])})
+        #         # {"type": "ineq",
+        #         #  "fun": lambda x: -x[3] + 2 * x[4]})
         _y = minimize(minus_log_likelihood,
                       y_start,
                       method="L-BFGS-B",