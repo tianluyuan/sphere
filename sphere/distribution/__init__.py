978d64af57a7a9b64b005fa09f207002c2c83a1b
Tianlu Yuan <tianlutheman@gmail.com>
2019-06-12 16:38:51 -0500

    implement saddle from kume and wood 2005

--- sphere/distribution/__init__.py
+++ sphere/distribution/__init__.py
@@ -5,4 +5,6 @@ from .distribution import fb84
 from .distribution import FB8Distribution
 from .distribution import fb8_mle
 from .distribution import kent_me
+from .saddle import spa
 del distribution
+del saddle
