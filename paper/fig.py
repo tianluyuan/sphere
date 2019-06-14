ec735dde17e3e771fb30929779ba8ba5d19a00be
Tianlu Yuan <tianlutheman@gmail.com>
2019-06-14 01:33:43 -0500

    line colors

--- paper/fig.py
+++ paper/fig.py
@@ -58,13 +58,12 @@ def approx_norm(kappa, eta):
     """
     betas = np.arange(kappa/10., kappa)
     plt.figure()
-    plt.plot(betas, [np.log(fb8(0,0,0,kappa,beta,eta).normalize()) for beta in betas], label='Series')
+    plt.plot(betas, [np.log(fb8(0,0,0,kappa,beta,eta).normalize()) for beta in betas], label='Series', color='k', linewidth=2)
     plt.plot(betas, [fb8(0,0,0,kappa,beta,eta)._approx_log_normalize() for beta in betas],
              linestyle='--',
              label='Approximate')
     plt.plot(betas, [spa(fb8(0,0,0,kappa,beta,eta)).log_c3() for beta in betas],
              linestyle=':',
-             color='k',
              label='Saddlepoint')
     plt.xlabel(r'$\beta$')
     plt.ylabel(r'$\ln c_6(\beta)$')
@@ -82,13 +81,12 @@ def numerical_norm(kappa, eta, alpha, rho):
     """
     betas = np.arange(kappa/10, kappa)
     plt.figure()
-    plt.plot(betas, [np.log(fb8(0,0,0,kappa,beta,eta,alpha,rho).normalize()) for beta in betas], label='Series')
+    plt.plot(betas, [np.log(fb8(0,0,0,kappa,beta,eta,alpha,rho).normalize()) for beta in betas], label='Series', color='k')
     plt.plot(betas, [np.log(fb8(0,0,0,kappa,beta,eta,alpha,rho)._nnormalize()) for beta in betas],
              linestyle='--',
              label='Numerical integration')
     plt.plot(betas, [spa(fb8(0,0,0,kappa,beta,eta)).log_c3() for beta in betas],
              linestyle=':',
-             color='k',
              label='Saddlepoint')
     plt.xlabel(r'$\beta$')
     plt.ylabel(r'$\ln c_8(\beta)$')
