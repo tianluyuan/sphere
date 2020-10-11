a1c289b91eade4a11f68e805ee204a5c43161231
Tianlu Yuan <tianlutheman@gmail.com>
2020-10-10 22:41:23 -0500

    add setup.py classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fb8',
-    version='0.2.2',
+    version='0.2.3',
     author='T. Yuan',
     author_email='tyuan@icecube.wisc.edu',
     description='Implementation of FB8, a generalization of the Kent (1982) and Bingham-Mardia (1978) distributions on a sphere',
@@ -17,4 +17,12 @@ setup(
     },
     python_requires='>=2.7',
     license=open('LICENSE').readline().split()[0],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        ],
     )
